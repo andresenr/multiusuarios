09126ef29521519df662512b3ab8c54c8f6d045f
andresenr <aarroyo440@gmail.com>
2022-10-27 11:06:55 -0500

    mi primer commit de este proyecto

---
+++ header.php
@@ -0,0 +1,18 @@
+	<nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="https://empresite.eleconomistaamerica.co/CLINICA-ALTA-COMPLEJIDAD-CARIBE-SAS.html">Bienvenido a nuestra pagina web safari africano</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href=""> ¡¡inicia seccion con el rol que tienes a cargo!!</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
